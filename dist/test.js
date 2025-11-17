3538fdb6525f1eed0a9c06836938acc802df81ed
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-11-17 21:32:00 +0330

    commit 1

--- dist/test.js
+++ dist/test.js
@@ -1,4 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "kaveh-khorshidi";
+const myObj = { id: 1, name: "Kaveh" };
+const myArray = [["kaveh"], [2003], ["khorshidi"]];
+const tuple = [1, "kaveh"];
+const myTuple = ["kaveh", 10];
+function sum(Code, Name) {
+    return `${Code} , ${Name}`;
+}
+console.log(sum("1", "Kaveh"));
+let isArray = [1, "koko"];
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,14 @@
-const name:string="kaveh-khorshidi"
+const myObj:object={id:1 , name:"Kaveh"}
+
+const myArray:(string | number)[][]=[["kaveh"] , [2003] , ["khorshidi"]]
+
+const tuple : [number , string]=[1 , "kaveh"]
+
+const myTuple : [string , number]=["kaveh" , 10]
+
+function sum (Code:string , Name:string):string {
+    return `${Code} , ${Name}`
+}
+console.log( sum("1" , "Kaveh") )
+
+let isArray:[number , string] = [1 , "koko"]
