c6e30a5c7ad16f1242d9c37568506c033c7f5204
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-11-30 22:34:22 +0330

    commit 8

--- dist/test.js
+++ dist/test.js
@@ -1,5 +1,2 @@
-let myFunction;
-const test1 = (a, b) => { return a + b; };
-myFunction = test1(1, 2);
 export {};
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++
@@ -1,7 +0,0 @@
-let myFunction : (num1:number , num2:number) => number
-
-
-const test1 = (a:number , b:number) => {return a + b}
-
-
-myFunction = test1(1 , 2)
